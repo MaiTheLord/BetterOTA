56935cc528df3ee0c667858a709b90605f93b801
MaiTheLord <63251610+MaiTheLord@users.noreply.github.com>
2022-02-15 11:36:10 +0200

    When starting the OTA it now closed the Terminal Server

--- src/ota/OTA.h
+++ src/ota/OTA.h
@@ -10,33 +10,13 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
 #include "../terminal/Terminal.h"
+#include "../terminal/TerminalBackend.h"
 
 namespace OTA {
     inline void init() {
         ArduinoOTA.onStart([]() {
             // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-            Terminal::println((ArduinoOTA.getCommand() == U_FLASH) ?
-                              "Started updating sketch" :
-                              "Started updating filesystem");
-        });
-
-        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-            Terminal::println(String(progress / (total / 100)) + "%");
-        });
-
-        ArduinoOTA.onEnd([]() {
-            Terminal::println("\nFinished");
-        });
-
-        ArduinoOTA.onError([](ota_error_t error) {
-            Terminal::println("Error " + String(error) + ": ");
-            switch (error) {
-                case OTA_AUTH_ERROR: Terminal::println("Auth Failed"); break;
-                case OTA_BEGIN_ERROR: Terminal::println("Begin Failed"); break;
-                case OTA_CONNECT_ERROR: Terminal::println("Connect Failed"); break;
-                case OTA_RECEIVE_ERROR: Terminal::println("Receive Failed"); break;
-                case OTA_END_ERROR: Terminal::println("End Failed"); break;
-            }
+            TerminalBackend::server.close();
         });
 
         ArduinoOTA.begin();
